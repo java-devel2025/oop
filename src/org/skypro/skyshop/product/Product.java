eb6b3052cb3078bf9240fb797915a8b7440327ad
java-devel2025 <kuznya9@mail.ru>
2025-10-17 18:53:08 +0700

    первое задание по ООП

---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,25 @@
+package org.skypro.skyshop.product;
+
+
+public class Product {
+    private final String name;
+    private final int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Геттеры для получения данных
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String toString() {
+        return name + ": " + price;
+    }
+}
